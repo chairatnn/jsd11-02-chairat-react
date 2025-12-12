9279da9b967407404c3add4bf850083cf6eb0809
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-12 23:33:38 +0700

    Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./views/Home";
 import User from "./views/User";
 import Admin from "./views/Admin";
 import Owner from "./views/Owner";
+// import ProductDetail from "./views/ProductDetail";
+// import FetchAPI_EmptyArray from "./views/FetchAPI_EmptyArray";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     errorElement: (
-      <div className="min-h-screen flex justify-center items-center">
-        <h1 className="text-4xl">404 - Page Not Found 🥲</h1>
-      </div>
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-4xl">404 - Page Not Found 🥲</h1>
+    </div>
     ),
     children: [
-      { path: "/", element: <Home /> },
+      { path: "/",element: <Home /> },
       { path: "user", element: <User /> },
       { path: "admin", element: <Admin /> },
       { path: "owner", element: <Owner /> },
+      // { path: "products/:productId", element: <ProductDetail /> },
+      // { path: "/FetchAPI_EmptyArray", element: <FetchAPI_EmptyArray /> },
     ],
   },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+     <RouterProvider router={router} />
+  );
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,26 +1,28 @@
-import { Link } from "react-router-dom";
-
 export default function Home() {
+  const handleButtonClick = () => {};
+
   return (
     <div className="bg-gray-300 justify-center">
-      <h1 className="text-5xl font-weight: bold front-size: x-large text-center py-4">
+      <h1 class="text-5xl font-weight: bold front-size: x-large text-center py-4">
         Generation Thailand
       </h1>
-      <h1 className="text-5xl font-weight: bold front-size: x-large text-center py-4">
+      <h1 class="text-5xl font-weight: bold front-size: x-large text-center py-4">
         React - Assessment
       </h1>
 
       <div className="flex justify-center space-x-40 px-3 py-10">
-        <Link to="/user">
-          <button className=" bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-xl shadow-md hover:bg-gray-400 transition duration-150">
-            User Home View
-          </button>
-        </Link>
-        <Link to="/admin">
-          <button className=" bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-xl shadow-md hover:bg-gray-400 transition duration-150">
-            Admin Home View
-          </button>
-        </Link>
+        <button 
+          onClick={handleButtonClick}
+          className=" bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-xl shadow-md hover:bg-gray-400 transition duration-150">
+          User Home View
+        </button>
+
+        <button
+          onClick={handleButtonClick}
+          className=" bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-xl shadow-md hover:bg-gray-400 transition duration-150">
+          Admin Home View
+        </button>
+
       </div>
     </div>
   );
