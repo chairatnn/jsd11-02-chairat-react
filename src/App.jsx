b122820ee6b58c23f636be68c96249e6f710dccf
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-12 10:24:25 +0700

    start assessment

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+export default function App() {
+
+
+
+  return (
+    <div className="min-h-screen flex justify-center bg-white">
+      <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
+        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+          Home
+        </h1>
+        <section className="w-full p-5 bg-amber-100 flex">
+          <ul className="list-inside list-disc flex-1">
+            <span className="font-semibold">Tech Stack:</span>
+            <li>Vite</li>
+            <li>React</li>
+            <li>JavaScript</li>
+            <li>Tailwind</li>
+          </ul>
+        </section>
+      </div>
+    </div>
+  );
+}
---
+++ src/views/Home.jsx
@@ -0,0 +1,10 @@
+// rafce
+import React from 'react'
+
+export const Home = () => {
+
+    
+  return (
+    <div>Home</div>
+  )
+}
