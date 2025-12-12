073a713f9709447bdb48719c5702818db80586e3
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-12 12:44:39 +0700

    structure3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+// import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./views/Home";
 import User from "./views/User";
